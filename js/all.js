256f2944b08781aaff1320769f63c60f33cb65b9
JordanTseng <jordan.ttc.design@gmail.com>
2022-03-20 22:54:10 +0800

    重構function

--- js/all.js
+++ js/all.js
@@ -9,29 +9,25 @@ let data;
 let showData = [];
 let category = '';
 
-this.axios.get(url).then((res) => {
-  data = res.data.filter((a) => a.作物名稱);
-  // TODO: 之後拆成 renderData 函式
+function renderData() {
   let str = '';
-  data.forEach((b) => {
-    // TODO: 改成 ES6 的 Template Literals (字面字串符)
-    const content = `<tr><td>${b['作物名稱']}</td><td>${b['市場名稱']}</td><td>${b['上價']}</td><td>${b['中價']}</td><td>${b['下價']}</td><td>${b['平均價']}</td><td>${b['交易量']}</td></tr>`;
+  showData.forEach((i) => {
+    const content = `<tr><td>${i['作物名稱']}</td><td>${i['市場名稱']}</td><td>${i['上價']}</td><td>${i['中價']}</td><td>${i['下價']}</td><td>${i['平均價']}}</td><td>${i['交易量']}}</td></tr>`;
     str += content;
   });
   table.innerHTML = str;
+}
+this.axios.get(url).then((res) => {
+  data = res.data.filter((a) => a.作物名稱);
+  renderData();
 });
 
 function filterCategory(e) {
   if (e.target.nodeName === 'BUTTON') {
     category = e.target.dataset.category;
     showData = data.filter((i) => i['種類代碼'] === category);
-    // TODO: 之後拆成 renderData 函式
-    let str = '';
-    showData.forEach((i) => {
-      const content = `<tr><td>${i['作物名稱']}</td><td>${i['市場名稱']}</td><td>${i['上價']}</td><td>${i['中價']}</td><td>${i['下價']}</td><td>${i['平均價']}}</td><td>${i['交易量']}}</td></tr>`;
-      str += content;
-    });
-    table.innerHTML = str;
+    renderData();
   }
 }
+
 filter.addEventListener('click', filterCategory);
